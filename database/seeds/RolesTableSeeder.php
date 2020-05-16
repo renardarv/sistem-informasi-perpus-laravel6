926c01f5b0a81c56e457dce6b8783ef03b2c7595
Arvy Renardi <renardiarvy@gmail.com>
2020-05-16 14:22:10 +0700

    Membuat role pendaftaran user

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create([
+            'name' => 'admin'
+        ]);
+
+        Role::create([
+            'name' => 'admin'
+        ]);
+    }
+}
